f690b94d7f225d260888d291d514d0395638e86d
navachetan <navachetan@navachetan-PC>
2021-12-29 17:53:07 +0530

    updated new java file

--- com.crm.SDET-27/src/test/java/practise/Demo.java
+++ com.crm.SDET-27/src/test/java/practise/Demo.java
@@ -6,6 +6,14 @@ public class Demo
 	public static void main(String[] args) 
 	{	
 		System.out.println("Welcome to git hub");
+		add();
+	}
+	
+	static void add()
+	{
+		int a=4;
+		int b=10;
+		int c=a+b;
 	}
 
 }
